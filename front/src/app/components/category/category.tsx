96388058055060988bdc3d1116a42bda4c2881fe
Joaco.M <joaco.martinez1480@gmail.com>
2025-05-21 20:15:29 -0300

    sin la palabra products

--- front/src/app/components/category/category.tsx
+++ front/src/app/components/category/category.tsx
@@ -68,7 +68,6 @@ function Category ({products} : ProductsProps) {
 </div>
         <div className="basis-5/6">
         <div>
-        <h2>Productos</h2>
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
         {currentProducts.map((product: IProduct) => (
             <Card key={product.id} product={product} />
